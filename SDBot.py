423bf5d065958acb108635aeae2527079b4af0f9
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-12 16:43:05 +0530

    Update SDBot.py

--- SDBot.py
+++ SDBot.py
@@ -1,11 +1,11 @@
 from pyrogram import Client
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "2121977275:AAHGkOfO62Py7g6Salx8v4BrBR8Ay2WXhyE")
 
-API_ID = int(os.environ.get("API_ID",12345))
+API_ID = int(os.environ.get("API_ID",7395896))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "cd3998ddf318dad74d7c506731bc0abc")
 
 if __name__ == "__main__" :
     plugins = dict(
